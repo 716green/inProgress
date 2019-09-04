695356ce57efcf7a62d28e2ba066ef00f813d846
716green <bbass9490@gmail.com>
2019-09-04 10:17:39 -0400

    Version 3.0 Stable
    Updated Version Information

--- PaymentPlanCalculator/PaymentPlanCalculator/Properties/AssemblyInfo.cs
+++ PaymentPlanCalculator/PaymentPlanCalculator/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PaymentPlanCalculator2")]
+[assembly: AssemblyTitle("Payment Plan Calculator V3")]
 [assembly: AssemblyDescription("Debt Collection Calculator App")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ashland Development, LLC")]
 [assembly: AssemblyProduct("PaymentPlanCalculator")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Ashland Development 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
